fc5ddc198a39f818d13bdb0f763d8b41d32c5473
gmimg <gmimg9@gmail.com>
2020-02-10 09:13:15 +0900

    repo test end

--- 200206 flask test/test site/app.py
+++ 200206 flask test/test site/app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, jsonify
 #from werkzeug import secure_filename
 #import keras
@@ -6,7 +7,7 @@ import pymysql
 app = Flask(__name__)
 
 
-db = pymysql.connect(host="127.0.0.1", user="ad",passwd="1234", db='tests', port=3306)
+
 
 
 
@@ -24,25 +25,45 @@ def info():
 def render_file():
     return render_template('upload.html')
 
+    
+@app.route('/backcolor', methods = ['GET', 'POST'])
+def backcolor():
+    if request.method == 'POST':
+        db = pymysql.connect(host="127.0.0.1", user="ad",passwd="1234", db='tests', port=3306)
+        cursor = db.cursor()
+        # 데이터베이스에서 사용자 이미지 가져오기
+        data = request.get_json()
+        item_sql = f"update USER_SETTING SET repo_color = '{data['backcolor']}' where USERID = 'admin'" 
+        cursor.execute(item_sql)
+        db.commit()
+        db.close()
+    return jsonify(data)
+
 
 #업로드 HTML 렌더링
-@app.route('/container')
+@app.route('/repository')
 def container():
+    db = pymysql.connect(host="127.0.0.1", user="ad",passwd="1234", db='tests', port=3306)
     cursor = db.cursor()
+    backcolor_sql = "select * from USER_SETTING where USERID = 'admin'" 
+    cursor.execute(backcolor_sql)
+    rows=cursor.fetchall()
+    row_headers=[x[0] for x in cursor.description]
+    backcolor = dict(zip(row_headers,rows[0]))['repo_color']
+    print(backcolor)
+
+
     # 데이터베이스에서 사용자 이미지 가져오기
-    item_sql = "select * from USER_ITEM"
+    item_sql = "select * from USER_ITEM where USERID = 'admin'" 
     cursor.execute(item_sql)
     rows=cursor.fetchall() 
     row_headers=[x[0] for x in cursor.description]
     json_data=[]
     for result in rows:
         json_data.append(dict(zip(row_headers,result)))
-    #print(json_data)
-    # json_return=json.dumps(json_data[0]) 
-    # print(json_return)
-    
     cursor.close()
-    return render_template('container.html',json_data = json_data)
+    db.close()
+    return render_template('repository.html',json_data = json_data, backcolor= backcolor)
 
 
 
@@ -53,19 +74,22 @@ def container():
 def image():
     if request.method == 'POST':
         data = request.get_json()
-        print(data)
-        print(data[0])
         for i in data:
             i = json.loads(i)
             item_name = i['item']
             x = i['x']
             y = i['y']
             z = i['z']
+            width = i['width']
+            height = i['height']
+
+            db = pymysql.connect(host="127.0.0.1", user="ad",passwd="1234", db='tests', port=3306)
             cursor = db.cursor()
-            item_sql = f"UPDATE USER_ITEM SET X_ITEM = {x}, Y_ITEM = {y}, Z_ITEM = {z} where ITEMID = '{item_name}'"
-             
+            item_sql = f"UPDATE USER_ITEM SET X_ITEM = {x}, Y_ITEM = {y}, Z_ITEM = {z}, WIDTH = {width}, HEIGHT = {height} where ITEMID = '{item_name}'"
             cursor.execute(item_sql)
-            cursor.fetchall() 
+            #cursor.fetchall()
+            db.commit()
+            db.close()
             
             
             
